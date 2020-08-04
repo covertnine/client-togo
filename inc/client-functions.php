d21ccd86e1d41b4fb4f1c67f6b13ab2e3c4cd5ec
tim <tim@covertnine.com>
2020-08-03 19:18:32 -0500

    adding local version of files for animation on scrolling

--- inc/client-functions.php
+++ inc/client-functions.php
@@ -24,9 +24,9 @@ if (!function_exists('client_scripts')) {
 
 		wp_enqueue_style('client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.min.css', array('c9-styles'));
 
-		wp_enqueue_script('gsap', '//s3-us-west-2.amazonaws.com/s.cdpn.io/16327/gsap-latest-beta.min.js', array('jquery'), '', true);
-		wp_enqueue_script('scrollto', '//s3-us-west-2.amazonaws.com/s.cdpn.io/16327/ScrollToPlugin3.min.js', array('jquery'), '', true);
-		wp_enqueue_script('scrollmagic', '//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js', '', '', true);
+		wp_enqueue_script('gsap', get_template_directory_uri() . '/client/client-assets/vendor/jquery.gsap.min.js', array('jquery'), '', true);
+		wp_enqueue_script('scrollto', get_template_directory_uri() . '/client/client-assets/vendor/plugins/ScrollToPlugin.min.js', array('jquery'), '', true);
+		wp_enqueue_script('scrollmagic', get_template_directory_uri() . '/client/client-assets/vendor/ScrollMagic/2.0.7/ScrollMagic.min.js', '', '', true);
 		wp_enqueue_script('history-js', get_template_directory_uri() . '/client/client-assets/vendor/history.js', array('jquery'), true);
 		wp_enqueue_script('client-scripts', get_template_directory_uri() . '/client/client-assets/custom-client.js', array('jquery'), '', true);
 		wp_enqueue_script('client-scripts', get_template_directory_uri() . '/client/client-assets/transitions.js', array('jquery'), '', true);
