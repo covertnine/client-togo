b7cb163dcafead346fb0380ce8e421ac321095f5
tim <tim@covertnine.com>
2020-09-08 18:04:45 -0500

    cleaning up function names

--- inc/client-editor.php
+++ inc/client-editor.php
@@ -6,8 +6,8 @@
  */
 
 /* add client compiled files to gutenberg editor */
-if (!function_exists('c9_client_editor_style')) {
-	function c9_client_editor_style()
+if (!function_exists('c9togo_client_editor_style')) {
+	function c9togo_client_editor_style()
 	{
 
 		$c9_default_font = get_theme_mod('c9_default_font', 'no');
@@ -20,5 +20,5 @@ if (!function_exists('c9_client_editor_style')) {
 		wp_enqueue_style('c9-client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.css');
 		wp_enqueue_style('c9-client-editor-styles', get_template_directory_uri() . '/client/client-assets/dist/client-editor.min.css', 99999);
 	}
-	add_action('enqueue_block_editor_assets', 'c9_client_editor_style', 99999999);
+	add_action('enqueue_block_editor_assets', 'c9togo_client_editor_style', 99999999);
 } //end if function exists
