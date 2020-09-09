fccc949518782fde6c40ac9bfd73f6638e13dd27
tim <tim@covertnine.com>
2020-09-09 11:31:16 -0500

    updating to latest version of gsap and scroll trigger

--- client-assets/custom-client.js
+++ client-assets/custom-client.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function () {
 	(function ($) {
 		if (jQuery("body.home").length) {
-			$("body").on(
+			$(".c9").on(
 				"click",
 				".nav-link[href^='/#'], .scroll-me a[href^='/#'], .dropdown-item[href^='/#'], a.scroll-me[href^='/#'], .scrollme[href^='/#'], .scrollme[href^='#']",
 				function (event) {
@@ -18,7 +18,7 @@ jQuery(document).ready(function () {
 							y: anchorID,
 							offsetY: 55
 						},
-						ease: Power2.easeOut
+						ease: "power1.out"
 					});
 
 					$(".navbar-collapse").toggleClass("show");
@@ -28,37 +28,37 @@ jQuery(document).ready(function () {
 	})(jQuery);
 });
 
-// add scenes for home scrolling nav links if user is on homepage
-if (jQuery("body.home").length) {
-	// init controller
-	var controller = new ScrollMagic.Controller({
-		globalSceneOptions: {
-			duration: "100%",
-			triggerHook: 0.5
-		}
-	});
+// // add scenes for home scrolling nav links if user is on homepage
+// if (jQuery("body.home").length) {
+// 	// init controller
+// 	var controller = new ScrollTrigger.create({
+// 		globalSceneOptions: {
+// 			duration: "100%",
+// 			triggerHook: 0.5
+// 		}
+// 	});
 
-	//set up array of links in nav linking to on-page anchors
-	var navLinks = [];
+// 	//set up array of links in nav linking to on-page anchors
+// 	var navLinks = [];
 
-	jQuery(
-		".nav-link[href^='/#'], .scroll-me a[href^='/#'], .dropdown-item[href^='/#']"
-	).each(function () {
-		// get all link IDs and put them in array from header and direct clicked scroll links
-		navLinks.push(jQuery(this).attr("href"));
-	});
+// 	jQuery(
+// 		".nav-link[href^='/#'], .scroll-me a[href^='/#'], .dropdown-item[href^='/#']"
+// 	).each(function () {
+// 		// get all link IDs and put them in array from header and direct clicked scroll links
+// 		navLinks.push(jQuery(this).attr("href"));
+// 	});
 
-	//loop through those links and add a scene for each that links up properly
-	var setSceneNum = navLinks.length;
+// 	//loop through those links and add a scene for each that links up properly
+// 	var setSceneNum = navLinks.length;
 
-	for (var i = 0; i < setSceneNum; i++) {
-		var anchorID = navLinks[i].substr(1);
-		var classLabel = navLinks[i].substr(2);
+// 	for (var i = 0; i < setSceneNum; i++) {
+// 		var anchorID = navLinks[i].substr(1);
+// 		var classLabel = navLinks[i].substr(2);
 
-		new ScrollMagic.Scene({
-				triggerElement: anchorID
-			})
-			.setClassToggle(".c9-" + classLabel, "nav-highlight") // add class toggle
-			.addTo(controller);
-	}
-}
+// 		new ScrollMagic.Scene({
+// 				triggerElement: anchorID
+// 			})
+// 			.setClassToggle(".c9-" + classLabel, "nav-highlight") // add class toggle
+// 			.addTo(controller);
+// 	}
+// }
--- client-assets/transitions.js
+++ client-assets/transitions.js
@@ -1,3 +1,27 @@
+(function (e, t) {
+	"use strict";
+	var n = e.History = e.History || {},
+		r = e.jQuery;
+	if (typeof n.Adapter != "undefined") throw new Error("History.js Adapter has already been loaded...");
+	n.Adapter = {
+		bind: function (e, t, n) {
+			r(e).bind(t, n)
+		},
+		trigger: function (e, t, n) {
+			r(e).trigger(t, n)
+		},
+		extractEventData: function (e, n, r) {
+			var i = n && n.originalEvent && n.originalEvent[e] || r && r[e] || t;
+			return i
+		},
+		onDomLoad: function (e) {
+			r(e)
+		}
+	}, typeof n.init != "undefined" && n.init()
+})(window);
+
+
+
 (function (window, undefined) {
 	var $wrap = jQuery("#smoothwrapper");
 
@@ -10,7 +34,7 @@
 
 		var pageTitle = this.title ? this.title : this.textContent;
 		pageTitle =
-			this.getAttribute("rel") === "home" ? pageTitle : pageTitle + " -” Togo";
+			this.getAttribute("rel") === "home" ? pageTitle : pageTitle;
 
 		History.pushState(null, pageTitle, this.href);
 	});
@@ -30,17 +54,17 @@
 	});
 
 	// record pageviews on Google Analytics if applicable
-	$wrap
-		.html($(elem).html())
-		.promise()
-		.done(function (res) {
-			// Make sure the new content is added, and the 'ga()' method is available.
-			if (typeof ga === "function" && res.length !== 0) {
-				//   ga("set", {
-				//     page: window.location.pathname,
-				//     title: state.title
-				//   });
-				//   ga("send", "pageview");
-			}
-		});
+	// $wrap
+	// 	.html($(elem).html())
+	// 	.promise()
+	// 	.done(function (res) {
+	// 		// Make sure the new content is added, and the 'ga()' method is available.
+	// 		if (typeof ga === "function" && res.length !== 0) {
+	// 			//   ga("set", {
+	// 			//     page: window.location.pathname,
+	// 			//     title: state.title
+	// 			//   });
+	// 			//   ga("send", "pageview");
+	// 		}
+	// 	});
 })(window);
