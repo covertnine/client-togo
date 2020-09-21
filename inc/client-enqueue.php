b01c26793a9ef23e5a044bfb5f4d34977e92b849
tim <tim@covertnine.com>
2020-09-21 17:03:02 -0500

    better styling for c9 blocks

--- inc/client-enqueue.php
+++ inc/client-enqueue.php
@@ -15,14 +15,14 @@ if (!function_exists('c9togo_client_scripts')) {
 		$c9_default_font = get_theme_mod('c9_default_font');
 
 		if ($c9_default_font == 'no') {
-			wp_enqueue_style('c9-font-default', 'https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap', array('c9-styles'));
+			wp_enqueue_style('c9togo-font-default', 'https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap', array('c9-styles'));
 		}
 
 		wp_enqueue_style('c9togo-client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.min.css', array('c9-styles'));
-		wp_enqueue_script('c9togo-gsap', get_template_directory_uri() . '/client/client-assets/vendor/gsap.min.js', array('jquery'), false, true);
-		wp_enqueue_script('c9togo-scrollto', get_template_directory_uri() . '/client/client-assets/vendor/plugins/ScrollToPlugin.min.js', array('jquery', 'c9togo-gsap'), false, true);
-		wp_enqueue_script('c9togo-scrolltrigger', get_template_directory_uri() . '/client/client-assets/vendor/plugins/ScrollTrigger.min.js', array('jquery', 'c9togo-gsap'), false, true);
-		wp_enqueue_script('c9togo-client-scripts', get_template_directory_uri() . '/client/client-assets/custom-client.js', array('jquery', 'c9-scripts', 'c9togo-gsap'), false, true);
+		wp_enqueue_script('c9-gsap', get_template_directory_uri() . '/client/client-assets/vendor/gsap.min.js', array('jquery'), false, true);
+		wp_enqueue_script('c9togo-scrollto', get_template_directory_uri() . '/client/client-assets/vendor/plugins/ScrollToPlugin.min.js', array('jquery', 'c9-gsap'), false, true);
+		wp_enqueue_script('c9togo-scrolltrigger', get_template_directory_uri() . '/client/client-assets/vendor/plugins/ScrollTrigger.min.js', array('jquery', 'c9-gsap'), false, true);
+		wp_enqueue_script('c9togo-client-scripts', get_template_directory_uri() . '/client/client-assets/custom-client.js', array('jquery', 'c9-scripts', 'c9-gsap'), false, true);
 		wp_add_inline_style('c9togo-client-styles', c9_togo_custom_css_output());
 
 	}
