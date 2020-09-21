029a51f58e4dac0cc6dfb6a1ef8afb47925759f1
tim <tim@covertnine.com>
2020-09-21 15:07:36 -0500

    updating sample content

--- inc/client-setup.php
+++ inc/client-setup.php
@@ -93,5 +93,192 @@ if (!function_exists('c9_client_setup')) {
                 'slug'    => 'color-black',
             ),
 		));
+
+				global $wp_filesystem;
+		// Initialize the WP filesystem, no more using 'file-put-contents' function
+		if (empty($wp_filesystem)) {
+			require_once(ABSPATH . '/wp-admin/includes/file.php');
+			require_once(ABSPATH . '/wp-admin/includes/class-wp-filesystem-base.php');
+			require_once(ABSPATH . '/wp-admin/includes/class-wp-filesystem-direct.php');
+			WP_Filesystem();
+		}
+
+		/**
+		 * Apearance > Customizer for fresh sites
+		 * Customizer Sample Content
+		 */
+		add_theme_support(
+			'starter-content',
+			array(
+				'attachments' => array(
+					'logo' => array(
+						'post_title' => _x('C9 Togo Logo', 'C9 Togo Logo', 'c9-togo'),
+						'file' => '/client/client-assets/img/c9-togo-logo-rough.svg',
+					),
+				),
+				'posts'	=> array(
+					'home'			=> array(
+						'comment_status'	=> 'closed',
+						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/home.html')
+					),
+					'about'			=> array(
+						'comment_status'	=> 'closed',
+						'post_type'			=> 'page',
+						'post_title'		=> __('About', 'c9-togo'),
+						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/about.html')
+					),
+					'setup'		=> array(
+						'comment_status'	=> 'closed',
+						'post_type'			=> 'page',
+						'post_title'		=> __('Setup', 'c9-togo'),
+						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/setup.html')
+					),
+					'styleguide'		=> array(
+						'comment_status'	=> 'closed',
+						'post_type'			=> 'page',
+						'post_title'		=> __('Style Guide', 'c9-togo'),
+						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/styleguide.html')
+					),
+					'blog'			=> array(
+						'post_content'			=> __('This page will show all of the blog posts once you have populated your database with blog items.', 'c9-togo')
+					),
+				),
+				'options'			=> array(
+					'show_on_front'		=> 'page',
+					'page_on_front'		=> '{{home}}',
+					'page_for_posts' 	=> '{{blog}}',
+					'blogname'			=> 'C9 Togo',
+					'blogdescription'	=> __('A blocks-based WordPress Theme for restaurants, breweries, or dispensaries offering online ordering, pickup, togo, or local delivery with the power of WooCommerce.', 'c9-togo'),
+				),
+				'theme_mods'		=> array(
+					'custom_logo' 			=> '{{logo}}',
+					'c9_show_search'		=> 'show',
+					'c9_copyright_content'	=> '&copy; 2020. <a href="https://www.covertnine.com" title="Web design company in Chicago" target="_blank">WordPress Website design by COVERT NINE</a>.',
+					'c9_default_font'		=> 'no',
+					'c9_author_visible'		=> 'hide',
+					'c9_blog_sidebar'		=> 'hide',
+					'c9_archive_sidebar'	=> 'hide',
+					'c9_show_social'		=> 'show',
+					'c9_twitter'			=> '#',
+					'c9_instagram'			=> '#',
+					'c9_spotify'			=> '#',
+					'c9_youtube'			=> '#',
+					'c9_linkedin'			=> '#',
+
+
+				),
+				'nav_menus'		=> array(
+					'primary'		=> array(
+						'name'			=>	__('Top Navigation Menu', 'c9-togo'),
+						'items'			=> array(
+							'page_home',
+							'page_about'	=> array(
+								'type'		=> 'post_type',
+								'object'	=> 'page',
+								'object_id'	=> '{{about}}',
+							),
+							'page_setup'	=> array(
+								'type'		=> 'post_type',
+								'object'	=> 'page',
+								'object_id'	=> '{{setup}}',
+							),
+							'page_styleguide'	=> array(
+								'type'		=> 'post_type',
+								'object'	=> 'page',
+								'object_id'	=> '{{styleguide}}',
+							),
+							'page_blog'
+						),
+					),
+				),
+				'widgets'	=> array(
+					'footerfull'	=> array(
+						'c9togo_resources'	=> array(
+							'text',
+							array(
+								'title'	=> __('Secondary Menu', 'c9-togo'),
+								'text'	=> '<ul id="menu-footer-resources" class="menu">
+									<li class="menu-item">
+										<a href="{{setup}}">Setup</a>
+									</li>
+									<li class="menu-item">
+										<a href="{{about}}">About</a>
+									</li>
+									<li class="menu-item">
+										<a href="{{blog}}">Blog</a>
+									</li>
+									<li class="menu-item">
+										<a href="{{styleguide}}">Style Guide</a>
+									</li>
+									<li class="menu-item">
+										<a href="{{home}}">Home</a>
+									</li>
+								</ul>'
+							)
+						),
+						'c9togo_company'	=> array(
+							'text',
+							array(
+								'title'	=> __('Company Menu', 'c9-togo'),
+								'text'	=> '<ul id="menu-footer-company" class="menu">
+									<li class="menu-item">
+										<a href="#">Our History</a>
+									</li>
+									<li class="menu-item">
+										<a href="/#browse-menu">Browse Menu</a>
+									</li>
+									<li class="menu-item">
+										<a href="/#order">Order Now</a>
+									</li>
+									<li class="menu-item">
+										<a href="#">Contact Us</a>
+									</li>
+									<li class="menu-item">
+										<a href="#">Privacy Policy</a>
+									</li>
+								</ul>'
+							)
+						),
+						'meta_custom' => array('meta', array(
+							'title'	=> __('WordPress Meta', 'c9-togo'),
+						)),
+						'c9togo_about' => array(
+							'text',
+							array(
+								'title'	=> __('About C9-Togo', 'c9-togo'),
+								'text'	=> __('Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.', 'c9-togo')
+							)
+						)
+					),
+					'right-sidebar'	=> array(
+						'search',
+						'c9togo_about' => array(
+							'text',
+							array(
+								'title'	=> __('About C9-Togo', 'c9-togo'),
+								'text'	=> __('Be sure to activate the <strong>C9 Blocks Plugin</strong> and <strong>C9 Admin Dashboard</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.', 'c9-togo')
+							)
+						),
+						'meta_custom' => array('meta', array(
+							'title'	=> 'Meta Widget',
+						)),
+					),
+					'left-sidebar'	=> array(
+						'search',
+						'c9togo_about' => array(
+							'text',
+							array(
+								'title'	=> __('About C9-Togo', 'c9-togo'),
+								'text'	=> __('Be sure to activate the <strong>C9 Blocks Plugin</strong> and <strong>C9 Admin Dashboard</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.', 'c9-togo')
+							)
+						),
+						'meta_custom' => array('meta', array(
+							'title'	=> __('Meta Widget', 'c9-togo'),
+						)),
+					),
+				),
+			)
+		);
+
 	}
 }
